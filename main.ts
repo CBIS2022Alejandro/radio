cb1e64d5cc90aff874fa74f521c72e6fd448d97c
CBIS2022Alejandro <123550863+CBIS2022Alejandro@users.noreply.github.com>
2023-03-27 15:55:38 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onGesture(Gesture.Shake, function () {
+    if (true) {
+    	
+    }
+})
+let angry = images.createImage(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . # # # .
+    # . . . #
+    `)
+let happy = images.createImage(`
+    . . . . .
+    . . . . .
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
