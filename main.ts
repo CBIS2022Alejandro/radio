63d0fd77cf9c0d10e800b57796e3a3707b3088ca
CBIS2022Alejandro <123550863+CBIS2022Alejandro@users.noreply.github.com>
2023-03-29 11:41:00 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,28 @@
+input.onGesture(Gesture.EightG, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . # # # .
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.clearScreen()
+})
 input.onGesture(Gesture.Shake, function () {
-    if (true) {
-    	
-    }
+    basic.showIcon(IconNames.Angry)
+    basic.pause(200)
+    basic.clearScreen()
 })
 let angry = images.createImage(`
     . . . . .
-    . . . . .
+    . # . # .
     . . . . .
     . # # # .
     # . . . #
     `)
 let happy = images.createImage(`
-    . . . . .
+    . # . # .
     . . . . .
     # . . . #
     # . . . #
