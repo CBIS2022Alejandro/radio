f753eac9dca74b3f55dacde1537d29e866b88232
CBIS2022Alejandro <123550863+CBIS2022Alejandro@users.noreply.github.com>
2023-04-12 11:58:02 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("¨crack!¨")
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        # # # # #
+        . # . # .
+        `)
+})
 input.onGesture(Gesture.SixG, function () {
+    basic.showIcon(IconNames.Angry)
+    basic.pause(5000)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
     basic.showLeds(`
         . # . # .
         . . . . .
@@ -6,11 +22,6 @@ input.onGesture(Gesture.SixG, function () {
         # # # # #
         # # # # #
         `)
-    basic.pause(5000)
-    basic.clearScreen()
-})
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Angry)
     basic.pause(5000)
     basic.clearScreen()
 })
@@ -38,3 +49,36 @@ let happy = images.createImage(`
     # . . . #
     # # # # #
     `)
+basic.forever(function () {
+    if (input.temperature() < 6 * 1 || input.temperature() < 21 * 1) {
+        if (input.lightLevel() < 50) {
+            basic.showLeds(`
+                . # . # .
+                . . . . .
+                . # # # .
+                . # . # .
+                . # # # .
+                `)
+        }
+    }
+})
+basic.forever(function () {
+    if (10 * 1 > input.temperature()) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . . . . .
+            # # # # #
+            # . # . #
+            `)
+    }
+    if (20 * 1 > input.temperature()) {
+        basic.showLeds(`
+            . . . # .
+            . . . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+})
